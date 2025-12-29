96a5772b201823cf24d72b9beb2585d13a971ec6
Chris (EJT) <chris.le-coquet-le-roux@mnhn.fr>
2025-12-29 12:31:20 +0100

    Update config.js

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -1,187 +1,63 @@
-/*
-This is the fil in which you configure the data widgets. Feel free to experiment with the configuration options. 
-But it is also completely okay to write and issue and ask for help to configure the widgets.
-You probably want to check out the documentation at https://hp-theme.gbif-staging.org/documentation-intro
-*/
+var siteTheme = gbifReactComponents.themeBuilder.extend({baseTheme: 'light', extendWith: {
+  primary: themeStyle.colors.primary,
+  fontSize: '16px'
+}});
+const publisherKeys = [
+  '7ce8aef0-9e92-11dc-8738-b8a03c50a862',
+];
+
 var siteConfig = {
-  "version": 3,
-  "pages": [ // which pages do we want to enable
-    {
-      "id": "occurrenceSearch"
-    },
-    {
-      "id": "occurrenceKey"
-    },
-    {
-      "id": "collectionSearch"
-    },
-    {
-      "id": "collectionKey"
-    },
-    {
-      "id": "datasetSearch"
-    },
-    {
-      "id": "datasetKey"
-    },
-    {
-      "id": "institutionSearch"
-    },
-    {
-      "id": "institutionKey"
-    },
-    {
-      "id": "literatureSearch"
-    }
-  ],
-  "disableInlineTableFilterButtons": false,
-  "availableCatalogues": [
-    // TODO: you should remove types you do not want to use
-    "OCCURRENCE",
-    "DATASET",
-    "COLLECTION",
-    "INSTITUTION",
-    "LITERATURE"
-  ],
-  "dataHeader": {
-    "enableApiPopup": false,
-    "enableInfoPopup": false
+  routes: {
+    alwaysUseHrefs: false,
+    enabledRoutes: ['occurrenceSearch', 'datasetSearch', 'datasetKey', 'literatureSearch', 'publisherKey', 'collectionKey'],
   },
-  "theme": {
-    "primary": "#001972",
-    "borderRadius": 3,
-    "stickyOffset": "0px"
+  dataset: {
+    rootFilter: {
+      publishingOrg: publisherKeys
+    },
+    excludedFilters: ['datasetType', 'hostingOrganizationKey', 'networkKey', 'publishingCountryCode', 'license', 'projectId', 'dwcaExtension'],
+    highlightedFilters: ['q', 'anyPublisherKey']
   },
-  "maps": {
-    "mapStyles": {
-      "defaultProjection": "MERCATOR",
-      "defaultMapStyle": "BRIGHT",
-      "options": {
-        "MERCATOR": [
-          "BRIGHT",
-          "NATURAL"
+  occurrence: {
+    excludedFilters: ['publishingCountryCode'],
+    highlightedFilters: ['taxonKey', 'country', 'year', 'datasetKey', 'publishingOrg'],
+    mapSettings: {
+      lat: 0,
+      lng: 0,
+      zoom: 1
+    },
+    // all the columns that are available to the user. This array defines the order they appear in. By default all all column are available.
+    availableTableColumns: ['features', 'country', 'stateProvince', 'locality', 'coordinates', 'year', 'eventDate', 'basisOfRecord', 'recordedBy', 'identifiedBy', 'recordNumber', 'catalogNumber', 'collectionCode', 'institutionCode', 'datasetKey', 'publishingOrg'],
+    defaultTableColumns: ['features', 'country', 'coordinates', 'year', 'catalogNumber', 'datasetKey'], // the columns showed by default. The order is not relevant, as it is defined in the list of available columns. The user can change what columns to show in the UI.
+    // You probably need help to configure the scope - so just ask
+    // for his demo site we only show Fungi (taxonKey=5). It use the predicate structure known from GBIF download API. 
+    // See https://www.gbif.org/developer/occurrence (long page without enough anchors - search for "Occurrence Download Predicates")
+    // The format is however slightly different, in that is use camelCase for keys instead of CONSTANT_CASE. 
+    rootPredicate: { type: 'in', key: 'publishingOrg', values: publisherKeys }, 
+    occurrenceSearchTabs: ['MAP', 'TABLE', 'DATASETS', 'CLUSTERS'] // what tabs should be shown
+    // see https://hp-theme.gbif-staging.org/data-exploration-config for more options
+  },
+  literature: {
+    rootFilter: {
+      predicate: {
+        type: 'and', predicates: [
+          {
+            type: 'in',
+            key: 'publishingOrganizationKey',
+            values: publisherKeys
+          },
+          {
+            type: 'equals',
+            key: 'relevance',
+            value: 'GBIF_USED'
+          },
+          {
+            type: 'equals',
+            key: 'peerReview',
+            value: 'true'
+          }
         ]
       }
     }
   },
-  "languages": [
-    {
-      "code": "en",
-      "localeCode": "en",
-      "label": "English",
-      "default": true,
-      "textDirection": "ltr",
-      "iso3LetterCode": "eng",
-      "cmsLocale": "en-GB",
-      "gbifOrgLocalePrefix": "",
-      "mapTileLocale": "en"
-    },
-    {
-      "code": "da",
-      "localeCode": "da",
-      "label": "Dansk",
-      "default": false,
-      "textDirection": "ltr",
-      "iso3LetterCode": "dan",
-      "cmsLocale": "en-GB",
-      "gbifOrgLocalePrefix": "",
-      "mapTileLocale": "en"
-    }
-  ],
-  "messages": {},
-  "occurrenceSearch": {
-    "scope": {
-      "type": "in",
-      "key": "publishingOrg",
-      "values": [
-        "760d5f24-4c04-40da-9646-1b2c935da502",
-        "2e7df380-8356-4533-bcb3-5459e23c794e",
-        "8e1a97a0-3ca8-11d9-8439-b8a03c50a862"
-      ]
-    },
-    "highlightedFilters": [
-      "taxonKey",
-      "verbatimScientificName",
-      "institutionKey",
-      "collectionKey",
-      "catalogNumber",
-      "recordedBy",
-      "identifiedBy"
-    ],
-    "excludedFilters": [
-      "occurrenceStatus",
-      "networkKey",
-      "hostingOrganizationKey",
-      "protocol",
-      "publishingCountry",
-      "institutionCode",
-      "collectionCode"
-    ],
-    "defaultEnabledTableColumns": [
-      "features",
-      "institutionKey",
-      "collectionKey",
-      "catalogNumber",
-      "country",
-      "year",
-      "recordedBy",
-      "identifiedBy"
-    ],
-    "tabs": [
-      "table",
-      "gallery",
-      "map",
-      "clusters",
-      "dashboard",
-      "download"
-    ],
-    "mapSettings": {
-      "lat": 52,
-      "lng": 12,
-      "zoom": 4.911544076366507
-    }
-  },
-  "collectionSearch": {
-    excludedFilters: ['country', 'active'],
-    // highlightedFilters: ['q', 'type', 'publishingOrg', 'license'],
-    // defaultTableColumns: ['title', 'description', 'publisher', 'type', 'occurrenceCount', 'literatureCount'],
-    scope: {
-      // TODO: you should add a scope here if you need search to be limited to a subset
-      // search filters have the format {field: [values]}
-      active: true
-    },
-  },
-  "institutionSearch": {
-    // excludedFilters: ['country', 'active'],
-    // highlightedFilters: ['q', 'type'],
-    // defaultTableColumns: ['title', 'type'],
-    scope: {
-      // TODO: you should add a scope here if you need search to be limited to a subset
-      // search filters have the format {field: [values]}
-      active: true
-    }
-  },
-  "datasetSearch": {
-    excludedFilters: ['publishingCountry', 'networkKey', 'projectId', 'hostingOrg'],
-    highlightedFilters: ['q', 'type', 'publishingOrg', 'license'],
-    scope: {
-      // TODO: you should add a scope here if you need search to be limited to a subset
-      // search filters have the format {field: [values]}
-      type: ['OCCURRENCE', 'CHECKLIST']
-    },
-  },
-  "publisherSearch": {},
-  "literatureSearch": {
-    "scope": {
-      // TODO: you should add a scope here if you need search to be limited to a subset
-      // literature search use the predicate format similar to occurrence search
-      "type": "in",
-      "key": "publishingOrganizationKey",
-      "values": [
-        "760d5f24-4c04-40da-9646-1b2c935da502",
-        "2e7df380-8356-4533-bcb3-5459e23c794e",
-        "8e1a97a0-3ca8-11d9-8439-b8a03c50a862"
-      ]
-    }
-  }
 }
